60eccbd0152a4a739d963dda5d0fb587fe668c97
Ana0609 <85016977+Ana0609@users.noreply.github.com>
2022-03-18 10:05:11 +0200

    lab3

--- spring2/src/main/java/com/unitbv/dependsOn/BeanA.java
+++ spring2/src/main/java/com/unitbv/dependsOn/BeanA.java
@@ -1,4 +1,11 @@
 package com.unitbv.dependsOn;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BeanA {
+
+
 }
